bdb9209c14a66ff27823a75ceb886b361fe29ff3
nikachu2012 <webmaster@nikachu.net>
2024-12-01 19:05:29 +0900

    [add] カーネルにフレームバッファの情報を送る構造体を追加

---
+++ kernel/graphics/frameBufferConfig.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stdint.h>
+
+enum pixelFormat
+{
+    PixelFormat_undefined,
+    /*
+        index | 0 | 1 | 2 | 3  | 4 | 5 | 6 | 7  | ... (Re = reserved)
+        data  | R | G | B | Re | R | G | B | Re | ...
+     */
+    PixelFormat_RGBResv8BitPerColor,
+    /*
+        index | 0 | 1 | 2 | 3  | 4 | 5 | 6 | 7  | ... (Re = reserved)
+        data  | B | G | R | Re | B | G | R | Re | ...
+    */
+    PixelFormat_BGRResv8BitPerColor,
+};
+
+struct frameBufferConfig
+{
+    uint8_t *frameBuffer;
+    uint32_t pixelPerScanLine;
+    uint32_t widthResolution;  // 画面の横幅
+    uint32_t heightResolution; // 画面の縦幅
+    enum pixelFormat pixelFormat;
+};
