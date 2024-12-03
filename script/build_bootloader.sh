0bb0f9cca103cad62d6675ef1f358d4f857abc11
nikachu2012 <webmaster@nikachu.net>
2024-12-03 19:45:37 +0900

    [add] 実行したフォルダにコピーする機能を追加

--- script/build_bootloader.sh
+++ script/build_bootloader.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-# edk2を用いてブートローダをビルドするスクリプト
+# edk2を用いてブートローダをビルドし
+# ビルド後のファイルを実行フォルダにコピーするスクリプト
 # edk2がビルドされていて、環境設定が終わっていることが前提
 # 終わっていない場合はdocs/howToCompile.mdを参照
 
 EDK2DIR="$HOME/workspace/edk2"
 
+DIR=$(pwd)
 cd $EDK2DIR
 source $EDK2DIR/edksetup.sh
 build
+
+cd $DIR
+cp $EDK2DIR/Build/hotaruBootloaderX64/DEBUG_CLANGPDB/X64/Loader.efi ./Loader.efi
