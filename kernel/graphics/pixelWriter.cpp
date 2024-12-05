3e5bd8971db7f7ae06b8fc552d7632df7240f290
nikachu2012 <webmaster@nikachu.net>
2024-12-05 21:39:58 +0900

    [update] コードが一貫性を持つよう変更

--- kernel/graphics/pixelWriter.cpp
+++ kernel/graphics/pixelWriter.cpp
@@ -20,44 +20,32 @@ constexpr uint32_t BGRResv8BitColorGen(const PixelRGBA &c)
     return (c.r << 16) | (c.g << 8) | c.b;
 }
 
-constexpr void drawRectBase(frameBufferConfig config, int x, int y, int width, int height, uint32_t color)
+constexpr size_t getAddr(const frameBufferConfig &c, int x, int y)
 {
-    uint32_t *startptr = nullptr;
-
-    // 上
-    startptr = config.frameBuffer + (config.pixelPerScanLine * y + x);
-    for (size_t dx = 0; dx < width; dx++)
-    {
-        startptr[dx] = color;
-    }
-
-    // 左右
-    startptr = config.frameBuffer;
-    for (int dy = y; dy < y + height; dy++)
-    {
-        startptr[config.pixelPerScanLine * dy + x] = color;
-        startptr[config.pixelPerScanLine * dy + x + width] = color;
-    }
-
-    // 下
-    startptr = config.frameBuffer + (config.pixelPerScanLine * (y + height) + x);
-    for (size_t dx = 0; dx < width + 1; dx++)
-    {
-        startptr[dx] = color;
-    }
+    return c.widthResolution * y + x;
 }
 
 void RGBResv8BitPerColorPixelWriter::drawPixel(int x, int y, const PixelTrueColor &c)
 {
-    auto p = config_.frameBuffer + (config_.pixelPerScanLine * y + x);
-
-    *p = RGBResv8BitColorGen(c);
+    config_.frameBuffer[getAddr(config_, x, y)] = RGBResv8BitColorGen(c);
 }
 
 void RGBResv8BitPerColorPixelWriter::drawRect(int x, int y, int width, int height, const PixelTrueColor &c)
 {
     auto color = RGBResv8BitColorGen(c);
-    drawRectBase(config_, x, y, width, height, color);
+    // 上下
+    for (size_t dx = 0; dx < width; dx++)
+    {
+        config_.frameBuffer[getAddr(config_, x + dx, y)] = color;
+        config_.frameBuffer[getAddr(config_, x + dx, y + height)] = color;
+    }
+
+    // 左右
+    for (int dy = 0; dy < height; dy++)
+    {
+        config_.frameBuffer[getAddr(config_, x, y + dy)] = color;
+        config_.frameBuffer[getAddr(config_, x + width, y + dy)] = color;
+    }
 }
 
 void RGBResv8BitPerColorPixelWriter::drawRectWithFill(int x, int y, int width, int height, const PixelTrueColor &c)
@@ -66,7 +54,7 @@ void RGBResv8BitPerColorPixelWriter::drawRectWithFill(int x, int y, int width, i
 
     for (size_t dy = 0; dy < height; dy++)
     {
-        auto startptr = config_.frameBuffer + (config_.pixelPerScanLine * dy + x);
+        auto startptr = config_.frameBuffer + getAddr(config_, x, y + dy);
         for (size_t dx = 0; dx < width; dx++)
         {
             startptr[dx] = color;
@@ -92,24 +80,35 @@ void RGBResv8BitPerColorPixelWriter::drawImageRGBA(int x, int y, const struct Pi
 
 void BGRResv8BitPerColorPixelWriter::drawPixel(int x, int y, const PixelTrueColor &c)
 {
-    auto p = config_.frameBuffer + (config_.pixelPerScanLine * y + x);
-
-    *p = BGRResv8BitColorGen(c);
+    config_.frameBuffer[getAddr(config_, x, y)] = BGRResv8BitColorGen(c);
 }
 
 void BGRResv8BitPerColorPixelWriter::drawRect(int x, int y, int width, int height, const PixelTrueColor &c)
 {
     auto color = RGBResv8BitColorGen(c);
-    drawRectBase(config_, x, y, width, height, color);
+
+    // 上下
+    for (size_t dx = 0; dx < width; dx++)
+    {
+        config_.frameBuffer[getAddr(config_, x + dx, y)] = color;
+        config_.frameBuffer[getAddr(config_, x + dx, y + height)] = color;
+    }
+
+    // 左右
+    for (int dy = 0; dy < height; dy++)
+    {
+        config_.frameBuffer[getAddr(config_, x, y + dy)] = color;
+        config_.frameBuffer[getAddr(config_, x + width, y + dy)] = color;
+    }
 }
 
 void BGRResv8BitPerColorPixelWriter::drawRectWithFill(int x, int y, int width, int height, const PixelTrueColor &c)
 {
     auto color = BGRResv8BitColorGen(c);
 
-    for (size_t dy = y; dy < y + height; dy++)
+    for (size_t dy = 0; dy < height; dy++)
     {
-        auto startptr = config_.frameBuffer + (config_.pixelPerScanLine * dy + x);
+        auto startptr = config_.frameBuffer + getAddr(config_, x, y + dy);
         for (size_t dx = 0; dx < width; dx++)
         {
             startptr[dx] = color;
