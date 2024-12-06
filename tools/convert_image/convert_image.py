a34ae84c9e9bca54c605dae64bff24bef204b3e2
nikachu2012 <webmaster@nikachu.net>
2024-12-07 03:15:03 +0900

    [update] 画像変換関数がソースファイルではなくヘッダファイルを返すように変更

--- tools/convert_image/convert_image.py
+++ tools/convert_image/convert_image.py
@@ -1,7 +1,7 @@
 import argparse
 from PIL import Image
 
-parser = argparse.ArgumentParser(description="画像をプログラムに埋め込めるC言語ソースに変換します")
+parser = argparse.ArgumentParser(description="画像をプログラムに埋め込めるC言語ヘッダに変換します")
 parser.add_argument("input", help="入力の画像ファイル", default="./cursor.png")
 parser.add_argument("label", help="変数名", default="cursor")
 
@@ -12,10 +12,13 @@ img = Image.open(args.input)
 img = img.convert("RGBA")
 data = list(img.getdata())
 
+print(f"#pragma once")
+
 print(f"const int {args.label}DataWidth = {img.width};")
 print(f"const int {args.label}DataHeight = {img.height};")
 
 print(f"const struct PixelRGBA {args.label}Data[{img.width*img.height}] = {{")
+print(f"    ", end="")
 
 for d in range(len(data) - 1):
     r, g, b, a = data[d]
