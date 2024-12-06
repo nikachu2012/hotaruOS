34bdf28a91c1ad26ef1d56bef3e95bee8d128401
nikachu2012 <webmaster@nikachu.net>
2024-12-07 03:21:46 +0900

    [fix] ２回目以降正しく入出力できなかったのでインラインでなくする

---
+++ kernel/native/io.cpp
@@ -0,0 +1,105 @@
+#include "io.hpp"
+
+// Intel記法を推進しています
+
+/**
+ * @brief 指定されたIOポートに8bit値を出力 (out binary)
+ *
+ * @param port ポート
+ * @param data 出力するデータ
+ */
+void outb(uint16_t port, uint8_t data)
+{
+    // out %al, %dx
+    // al -> dx
+    asm("out %b0, %w1"
+        :
+        : "a"(data), "d"(port));
+}
+
+/**
+ * @brief 指定されたIOポートに16bit値を出力 (out word)
+ *
+ * @param port ポート
+ * @param data 出力するデータ
+ */
+void outw(uint16_t port, uint16_t data)
+{
+    // out %ax, %dx
+    // ax -> dx
+    asm("out %w0, %w1"
+        :
+        : "a"(data), "d"(port));
+}
+
+/**
+ * @brief 指定されたIOポートに32bit値を出力 (out double word)
+ *
+ * @param port ポート
+ * @param data 出力するデータ
+ */
+void outdw(uint16_t port, uint32_t data)
+{
+    // out %eax, %dx
+    // eax -> dx
+    asm("out %k0, %w1"
+        :
+        : "a"(data), "d"(port));
+}
+
+/**
+ * @brief 指定されたIOポートから8bit値を入力 (in byte)
+ *
+ * @param port ポート
+ * @return uint8_t 入力されたデータ
+ */
+uint8_t inb(uint16_t port)
+{
+    uint8_t data;
+
+    // in %dx, %al
+    // dx -> al
+    asm("in %w1, %b0"
+        : "=a"(data)
+        : "d"(port));
+
+    return data;
+}
+
+/**
+ * @brief 指定されたIOポートから16bit値を入力 (in word)
+ *
+ * @param port ポート
+ * @return uint16_t 入力されたデータ
+ */
+uint16_t inw(uint16_t port)
+{
+    uint16_t data;
+
+    // in %dx, %ax
+    // dx -> ax
+    asm("in %w1, %w0"
+        : "=a"(data)
+        : "d"(port));
+
+    return data;
+}
+
+/**
+ * @brief 指定されたIOポートから32bit値を入力 (in double word)
+ *
+ * @param port ポート
+ * @return uint32_t 入力されたデータ
+ */
+uint32_t indw(uint16_t port)
+{
+    uint32_t data;
+
+    // in %dx, %eax
+    // dx -> eax
+    asm("in %w1, %k0"
+        : "=a"(data)
+        : "d"(port));
+
+    return data;
+}
--- kernel/native/io.hpp
+++ kernel/native/io.hpp
@@ -10,14 +10,7 @@
  * @param port ポート
  * @param data 出力するデータ
  */
-inline void outb(uint16_t port, uint8_t data)
-{
-    // out %al, %dx
-    // al -> dx
-    asm("out %b0, %w1"
-        :
-        : "a"(data), "d"(port));
-}
+void outb(uint16_t port, uint8_t data);
 
 /**
  * @brief 指定されたIOポートに16bit値を出力 (out word)
@@ -25,14 +18,7 @@ inline void outb(uint16_t port, uint8_t data)
  * @param port ポート
  * @param data 出力するデータ
  */
-inline void outw(uint16_t port, uint16_t data)
-{
-    // out %ax, %dx
-    // ax -> dx
-    asm("out %w0, %w1"
-        :
-        : "a"(data), "d"(port));
-}
+void outw(uint16_t port, uint16_t data);
 
 /**
  * @brief 指定されたIOポートに32bit値を出力 (out double word)
@@ -40,33 +26,14 @@ inline void outw(uint16_t port, uint16_t data)
  * @param port ポート
  * @param data 出力するデータ
  */
-inline void outdw(uint16_t port, uint32_t data)
-{
-    // out %eax, %dx
-    // eax -> dx
-    asm("out %k0, %w1"
-        :
-        : "a"(data), "d"(port));
-}
-
+void outdw(uint16_t port, uint32_t data);
 /**
  * @brief 指定されたIOポートから8bit値を入力 (in byte)
  *
  * @param port ポート
  * @return uint8_t 入力されたデータ
  */
-inline uint8_t inb(uint16_t port)
-{
-    uint8_t data;
-
-    // in %dx, %al
-    // dx -> al
-    asm("in %w1, %b0"
-        : "=a"(data)
-        : "d"(port));
-
-    return data;
-}
+uint8_t inb(uint16_t port);
 
 /**
  * @brief 指定されたIOポートから16bit値を入力 (in word)
@@ -74,18 +41,7 @@ inline uint8_t inb(uint16_t port)
  * @param port ポート
  * @return uint16_t 入力されたデータ
  */
-inline uint16_t inw(uint16_t port)
-{
-    uint16_t data;
-
-    // in %dx, %ax
-    // dx -> ax
-    asm("in %w1, %w0"
-        : "=a"(data)
-        : "d"(port));
-
-    return data;
-}
+uint16_t inw(uint16_t port);
 
 /**
  * @brief 指定されたIOポートから32bit値を入力 (in double word)
@@ -93,15 +49,4 @@ inline uint16_t inw(uint16_t port)
  * @param port ポート
  * @return uint32_t 入力されたデータ
  */
-inline uint32_t indw(uint16_t port)
-{
-    uint32_t data;
-
-    // in %dx, %eax
-    // dx -> eax
-    asm("in %w1, %k0"
-        : "=a"(data)
-        : "d"(port));
-
-    return data;
-}
+uint32_t indw(uint16_t port);
